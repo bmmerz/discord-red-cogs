3bdd64b2e8771165aa0859b6a233d70dc065cab3
unknown <bmmerz@gmail.com>
2025-12-22 15:25:54 -0500

    updated init.py

--- stonk/__init__.py
+++ stonk/__init__.py
@@ -1,3 +1,18 @@
+from redbot.core import errors
+import importlib
+import sys
+
+try:
+    import yfinance
+except ModuleNotFoundError:
+    raise errors.CogLoadError(
+        "yfinance could not be imported"
+    )
+try:
+    importlib.import_module("yfinance")
+except ModuleNotFoundError:
+    pass
+
 from .stonk import Stonk
 
 async def setup(bot):
