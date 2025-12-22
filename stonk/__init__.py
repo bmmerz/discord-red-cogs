5250e2b611b8fa80e6b898f89a28142409ac3abc
unknown <bmmerz@gmail.com>
2025-12-22 15:29:56 -0500

    updated init.py

--- stonk/__init__.py
+++ stonk/__init__.py
@@ -1,17 +1,28 @@
-from redbot.core import errors
-import importlib
+#from redbot.core import errors
+#import importlib
+#import sys
+
+#try:
+#    import yfinance
+#except ModuleNotFoundError:
+#    raise errors.CogLoadError(
+#        "yfinance could not be imported"
+#    )
+#try:
+#    importlib.import_module("yfinance")
+#except ModuleNotFoundError:
+#    pass
+
+import subprocess
 import sys
 
-try:
-    import yfinance
-except ModuleNotFoundError:
-    raise errors.CogLoadError(
-        "yfinance could not be imported"
-    )
-try:
-    importlib.import_module("yfinance")
-except ModuleNotFoundError:
-    pass
+def install_requirements():
+    try:
+        subprocess.call('pip install -r requirements.txt', shell=True)
+    except subprocess.CalledProcessError:
+        print("error installing requirements")
+
+install_requirements()
 
 from .stonk import Stonk
 
