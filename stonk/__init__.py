34159e6401f0ab8a0776beb56eff113fef5fff89
unknown <bmmerz@gmail.com>
2025-12-26 11:30:14 -0500

    added .gitignore

--- stonk/__init__.py
+++ stonk/__init__.py
@@ -1,29 +1,3 @@
-#from redbot.core import errors
-#import importlib
-#import sys
-
-#try:
-#    import yfinance
-#except ModuleNotFoundError:
-#    raise errors.CogLoadError(
-#        "yfinance could not be imported"
-#    )
-#try:
-#    importlib.import_module("yfinance")
-#except ModuleNotFoundError:
-#    pass
-
-import subprocess
-import sys
-
-def install_requirements():
-    try:
-        subprocess.call('pip install -r requirements.txt', shell=True)
-    except subprocess.CalledProcessError:
-        print("error installing requirements")
-
-install_requirements()
-
 from .stonk import Stonk
 
 async def setup(bot):
